727cb92cdafee692987ea877b5293c8919851a78
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-04-05 06:59:53 -0400

    CI

--- Package.swift
+++ Package.swift
@@ -1,23 +1,31 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "Glyph",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Glyph",
-            targets: ["Glyph"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Glyph"),
-        .testTarget(
-            name: "GlyphTests",
-            dependencies: ["Glyph"]),
-    ]
+	name: "Glyph",
+	platforms: [
+		.macOS(.v10_15),
+		.macCatalyst(.v13),
+		.iOS(.v13),
+		.tvOS(.v13),
+		.visionOS(.v1),
+	],
+	products: [
+		.library(name: "Glyph", targets: ["Glyph"]),
+	],
+	targets: [
+		.target(name: "Glyph"),
+		.testTarget(name: "GlyphTests", dependencies: ["Glyph"]),
+	]
 )
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency"),
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
